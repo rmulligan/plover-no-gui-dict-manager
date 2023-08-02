3425b7796ff245364d0a7d1bc98f40a67911b5b5
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 13:30:55 -0700

    Find last word in dictionaries

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -5,11 +5,19 @@ fingerspelled word with its steno equivalent. If the word is not in
 the user's dictionary, it prompts the user to enter a new chord for
 that word.
 """
+from plover.engine import StenoEngine
+import plover.log as log
 
-from plover.formatting import RetroFormatter
-import plover.log
+def looksert(engine: StenoEngine, _arg: str):
+    # Get the last word from the buffer
+    last_word = engine.translator_state[-1].english[0]
 
-def looksert(engine, _argument):
-    retro_formatter = RetroFormatter(engine.translator_state.prev())
-    last_word = retro_formatter.last_words(count=1)
-    plover.log.info("Last word: " + str(last_word))
+    # Perform the lookup
+    chord = engine.lookup((last_word,))
+
+    if chord:
+        # Log the chord if it was found in the dictionary
+        log.info("Found chord for word {}: {}".format(last_word, chord))
+    else:
+        # If the chord is not found, log a message indicating this
+        log.info("No chord found for word {}".format(last_word))
