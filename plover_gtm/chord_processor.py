d5e5a8ef3f3abebec08c23df3019eac43829cef0
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 13:36:59 -0700

    Looking up last word

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -6,11 +6,15 @@ the user's dictionary, it prompts the user to enter a new chord for
 that word.
 """
 from plover.engine import StenoEngine
+from plover.formatting import RetroFormatter
 import plover.log as log
 
 def looksert(engine: StenoEngine, _arg: str):
+    # Create a RetroFormatter instance with the previous translations from the engine
+    formatter = RetroFormatter(engine.translator_state.translations)
+
     # Get the last word from the buffer
-    last_word = engine.translator_state[-1].english[0]
+    last_word = formatter.last_words(1)[0]
 
     # Perform the lookup
     chord = engine.lookup((last_word,))
