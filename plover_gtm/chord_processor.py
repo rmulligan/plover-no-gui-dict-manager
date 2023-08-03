839fcb7f36b276ceb24037cb086e6e7e0acb131a
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 23:26:17 -0700

    testing

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -2,11 +2,13 @@ from plover import log
 from plover_gtm.plugin import gtm
 
 def inline_lookup_chord(translator, _stroke, _):
-    last_word = gtm.get_last_word()
-    steno_strokes_list = translator.get_dictionary().casereverse_lookup(last_word)
+    # last_word = gtm.get_last_word()
+    # steno_strokes_list = translator.get_dictionary().casereverse_lookup(last_word)
 
-    if not steno_strokes_list:
-        log.info("No steno strokes found for word '{}'".format(last_word))
-    else:
-        shortest_steno_strokes = min(steno_strokes_list, key=len)
-        log.info("Shortest steno strokes for word '{}': {}".format(last_word, shortest_steno_strokes))
+    # if not steno_strokes_list:
+    #     log.info("No steno strokes found for word '{}'".format(last_word))
+    # else:
+    #     shortest_steno_strokes = min(steno_strokes_list, key=len)
+    #     log.info("Shortest steno strokes for word '{}': {}".format(last_word, shortest_steno_strokes))
+    log.info("Inline lookup chord")
+    pass
