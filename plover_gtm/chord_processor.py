66d864bdf7a440b5c1a4ecafaa2b01e9da695ad0
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 16:29:04 -0700

    Dumping words to file on macro trigger

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -1,22 +1,8 @@
-from plover import log
-from plover.formatting import _Action
-from plover_gtm.plugin import PloverGtmPlugin
+import os
 
 def inline_lookup_chord(translator, _stroke, _):
     last_word = PloverGtmPlugin.get_instance().get_last_word()
-    all_dictionaries = translator.get_dictionary()
-    steno_strokes_list = all_dictionaries.reverse_lookup(last_word)
 
-    if not steno_strokes_list:
-        log.info("No steno strokes found for word '{}'".format(last_word))
-    else:
-        shortest_steno_strokes = min(steno_strokes_list, key=len)
-        log.info("Shortest steno strokes for word '{}': {}".format(last_word, shortest_steno_strokes))
-
-        # Send backspaces to delete the last word
-        backspaces = '\b' * len(last_word)
-        PloverGtmPlugin.get_instance()._engine.send_string(backspaces)
-
-        # Send the brief
-        brief = '/' + '/'.join(shortest_steno_strokes) + '/'
-        PloverGtmPlugin.get_instance()._engine.send_string(brief)
+    # Append the word to the file
+    with open(os.path.expanduser('/root/.config/plover/fingerspelled_words.log'), 'a') as f:
+        f.write(last_word + '\n')
