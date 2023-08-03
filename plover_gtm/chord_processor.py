d5bbc3bed692109cd53b3c942a45a46cb57df285
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 21:47:14 -0700

    changed name to resolve conflicts

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -2,7 +2,7 @@ from plover.engine import Translator
 from plover.steno import Stroke
 from plover_gtm_plugin import last_word_tracker
 
-def lookup_chord(translator, _stroke, _):
+def inline_lookup_chord(translator, _stroke, _):
     last_word = last_word_tracker.get_last_word()
 
     steno_strokes_list = translator.get_dictionary().casereverse_lookup(last_word)
