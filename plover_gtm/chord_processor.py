1edcdc4123fcac79b20e5d6c4b3e34b3a5fa4bd0
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 20:39:48 -0700

    get translations

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -2,12 +2,16 @@ from plover.engine import Translator
 from plover.steno import Stroke
 
 def lookup_chord(translator, _stroke, _):
-    last_word = translator.get_last_translation().english.lower()
-    steno_strokes_list = translator.get_dictionary().casereverse_lookup(last_word)
+    if len(translator.translations) > 0:
+        last_translation = translator.translations[-1].english.lower()
+    else:
+        last_translation = ""
+
+    steno_strokes_list = translator.get_dictionary().casereverse_lookup(last_translation)
 
     if not steno_strokes_list:
-        return "No steno strokes found for word '{}'".format(last_word)
+        return "No steno strokes found for word '{}'".format(last_translation)
 
     shortest_steno_strokes = min(steno_strokes_list, key=len)
 
-    return "Shortest steno strokes for word '{}': {}".format(last_word, shortest_steno_strokes)
+    return "Shortest steno strokes for word '{}': {}".format(last_translation, shortest_steno_strokes)
