0b42a236d905a7b51ae208e927993809087eee7e
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-02 22:23:44 -0700

    Logging

--- plover_gtm/plugin.py
+++ plover_gtm/plugin.py
@@ -1,13 +1,13 @@
 from plover.engine import StenoEngine
 from .last_word_tracker import LastWordTracker
+from plover import log
 
 class PloverGtmPlugin:
     def __init__(self, engine: StenoEngine):
+        log.info('PloverGtmPlugin __init__ called')
         self.engine = engine
         self.last_word_tracker = LastWordTracker()
 
     def start(self):
+        log.info('PloverGtmPlugin start called')
         self.engine.hook_connect("translated", self.last_word_tracker.on_translated)
-
-def get_instance():
-    return PloverGtmPlugin(StenoEngine())
