3f02f987d559c6384a6cb811d3e545e17fcb29cf
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 22:09:49 -0700

    Tests

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -3,15 +3,14 @@ from plover import log
 
 def start_capture(engine, stroke, command):
     log.info("start_capture")
-    #    PloverGtmPlugin.get_instance().start_capture()
     pass
 
-def end_capture(engine, stroke, command):
-    log.info("end_capture")
-    # PloverGtmPlugin.get_instance().end_capture()
-    pass
+# def end_capture(engine, stroke, command):
+#     log.info("end_capture")
+#     # PloverGtmPlugin.get_instance().end_capture()
+#     pass
 
-def abort_capture(engine, stroke, command):
-    log.info("abort_capture")
-    # PloverGtmPlugin.get_instance().abort_capture()
-    pass
+# def abort_capture(engine, stroke, command):
+#     log.info("abort_capture")
+#     # PloverGtmPlugin.get_instance().abort_capture()
+#     pass
