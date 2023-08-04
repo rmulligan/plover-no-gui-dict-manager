54db896870991cab4cf9b54b088379578c043aef
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 23:05:54 -0700

    Indentation issue

--- plover_gtm/plugin.py
+++ plover_gtm/plugin.py
@@ -35,17 +35,17 @@ class PloverGtmPlugin:
         self._engine.hook_disconnect("translated", self.on_translation_added)
 
     def on_translation_added(self, old, new):
-    log.info("PloverGtmPlugin: on_translation_added triggered")
-    if new:
-        if self._capturing:
-            if len(new[-1].text) == 1:  # check if it's a single letter
-                # If it's a single letter, append it to the current word.
-                if self._capture_buffer:  # check if there's a current word
-                    self._capture_buffer[-1] += new[-1].text
-                else:  # if there's no current word, start a new one
+        log.info("PloverGtmPlugin: on_translation_added triggered")
+        if new:
+            if self._capturing:
+                if len(new[-1].text) == 1:  # check if it's a single letter
+                    # If it's a single letter, append it to the current word.
+                    if self._capture_buffer:  # check if there's a current word
+                        self._capture_buffer[-1] += new[-1].text
+                    else:  # if there's no current word, start a new one
+                        self._capture_buffer.append(new[-1].text)
+                else:  # if it's not a single letter, treat it as a new word
                     self._capture_buffer.append(new[-1].text)
-            else:  # if it's not a single letter, treat it as a new word
-                self._capture_buffer.append(new[-1].text)
 
     def start_capture(self):
         log.info("PloverGtmPlugin: start_capture")
@@ -55,7 +55,7 @@ class PloverGtmPlugin:
     def end_capture(self):
         log.info("PloverGtmPlugin: end_capture")
         with open(os.path.expanduser("~/.config/plover/typed_words.log"), "a") as f:
-            f.write("".join(self._capture_buffer))  # Join the words with spaces
+            f.write(" ".join(self._capture_buffer))  # Join the words with spaces
             f.write("\n")  # Write a newline character after each capture session
         self._capturing = False
         self._capture_buffer = []
