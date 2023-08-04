932b38578fc8280c08870f579cec4583d0790bdc
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 22:29:35 -0700

    Restore capture

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -5,12 +5,10 @@ def start_capture(engine, stroke, command):
     log.info("start_capture")
     PloverGtmPlugin.get_instance().start_capture()
 
-# def end_capture(engine, stroke, command):
-#     log.info("end_capture")
-#     # PloverGtmPlugin.get_instance().end_capture()
-#     pass
+def end_capture(engine, stroke, command):
+    log.info("end_capture")
+    PloverGtmPlugin.get_instance().end_capture()
 
-# def abort_capture(engine, stroke, command):
-#     log.info("abort_capture")
-#     # PloverGtmPlugin.get_instance().abort_capture()
-#     pass
+def abort_capture(engine, stroke, command):
+    log.info("abort_capture")
+    PloverGtmPlugin.get_instance().abort_capture()
