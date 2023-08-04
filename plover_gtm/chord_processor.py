2e7fd9c5a3ca7e7521faa9fd7e2b62826484bcb4
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 17:37:15 -0700

    Append new line character to each log entry

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -2,9 +2,8 @@ from plover_gtm.plugin import PloverGtmPlugin
 
 import os
 
-def inline_lookup_chord(translator, _stroke, _):
+def inline_lookup_chord(engine, _stroke, _):
     last_word = PloverGtmPlugin.get_instance().get_last_word()
-
-    # Append the word to the file
-    with open(os.path.expanduser('/root/.config/plover/fingerspelled_words.log'), 'a') as f:
-        f.write(last_word + '\n')
+    if last_word:
+        with open(os.path.expanduser("~/.config/plover/typed_words.log"), 'a') as log_file:
+            log_file.write(last_word + '\n')
