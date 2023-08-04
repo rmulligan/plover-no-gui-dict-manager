0e883cf638da204bec138762731e0abd9862cdd0
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2023-08-03 21:55:05 -0700

    Fixed macro strokes

--- plover_gtm/chord_processor.py
+++ plover_gtm/chord_processor.py
@@ -3,12 +3,12 @@ from plover_gtm.plugin import PloverGtmPlugin
 
 def start_capture(engine, stroke, command):
     log.info("start_capture")
-#    PloverGtmPlugin.get_instance().start_capture()
+    PloverGtmPlugin.get_instance().start_capture()
 
 def end_capture(engine, stroke, command):
     log.info("end_capture")
-#    PloverGtmPlugin.get_instance().end_capture()
+    PloverGtmPlugin.get_instance().end_capture()
 
 def abort_capture(engine, stroke, command):
-    # log.info("abort_capture")
+    log.info("abort_capture")
     PloverGtmPlugin.get_instance().abort_capture()
